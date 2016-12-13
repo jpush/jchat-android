7df86d1ea178bc65d5836a98385622caa754a53f
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2016-12-13 13:47:26 +0800

    add WebView to show doc, excel or other document

---
+++ JChat/src/io/jchat/android/activity/DocumentBrowseActivity.java
@@ -0,0 +1,17 @@
+package io.jchat.android.activity;
+
+
+import android.os.Bundle;
+
+import io.jchat.android.R;
+
+public class DocumentBrowseActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_browse);
+
+                
+    }
+}
